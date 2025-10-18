9cb6621875113a05d11d3706afa1ee73418ed215
iamethanm <guoyang.mao@mail.utoronto.ca>
2025-10-18 00:09:48 -0400

    Refactor comments in BreedFetcher interface
    Updated comments for clarity and conciseness.

--- src/main/java/dogapi/BreedFetcher.java
+++ src/main/java/dogapi/BreedFetcher.java
@@ -2,21 +2,13 @@ package dogapi;
 
 import java.util.List;
 
-/**
- * Interface for the service of getting sub breeds of a given dog breed.
- */
+/** Interface for the service of getting sub breeds of a given dog breed. */
 public interface BreedFetcher {
 
-    /**
-     * Fetch the list of sub breeds for the given breed.
-     * @param breed the breed to fetch sub breeds for
-     * @return list of sub breeds for the given breed
-     * @throws BreedNotFoundException if the breed does not exist
-     */
+    /** Fetch the list of sub breeds for the given breed. */
     List<String> getSubBreeds(String breed) throws BreedNotFoundException;
 
-    // Task 4: make this a checked exception.
-    // a class defined in an interface is public AND static
+    // Checked exception
     class BreedNotFoundException extends Exception {
         public BreedNotFoundException(String breed) {
             super("Breed not found: " + breed);
