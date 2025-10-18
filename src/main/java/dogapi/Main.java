5f1d85122eeef6f6f7e0addd0f81c80a87c01f50
iamethanm <guoyang.mao@mail.utoronto.ca>
2025-10-18 00:10:50 -0400

    Remove unnecessary package declaration in Main.java

--- src/main/java/dogapi/Main.java
+++ src/main/java/dogapi/Main.java
@@ -1,11 +1,8 @@
-package dogapi;
-
-public final class Main {
-    public static int getNumberOfSubBreeds(String breed, BreedFetcher breedFetcher) {
-        try {
-            return breedFetcher.getSubBreeds(breed).size();
-        } catch (BreedFetcher.BreedNotFoundException e) {
-            return 0;
-        }
+public static int getNumberOfSubBreeds(String breed, BreedFetcher breedFetcher) {
+    try {
+        return breedFetcher.getSubBreeds(breed).size();
+    } catch (BreedFetcher.BreedNotFoundException e) {
+        return 0;
     }
 }
+
