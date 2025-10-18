eaa3b8c4ffed079d7b9b9dc188eb844983d1a78d
iamethanm <guoyang.mao@mail.utoronto.ca>
2025-10-18 00:14:11 -0400

    Refactor Main class structure and method visibility

--- src/main/java/dogapi/Main.java
+++ src/main/java/dogapi/Main.java
@@ -1,8 +1,13 @@
-public static int getNumberOfSubBreeds(String breed, BreedFetcher breedFetcher) {
-    try {
-        return breedFetcher.getSubBreeds(breed).size();
-    } catch (BreedFetcher.BreedNotFoundException e) {
-        return 0;
+package dogapi;
+
+public final class Main {
+    private Main() {}
+
+    public static int getNumberOfSubBreeds(String breed, BreedFetcher breedFetcher) {
+        try {
+            return breedFetcher.getSubBreeds(breed).size();
+        } catch (BreedFetcher.BreedNotFoundException e) {
+            return 0;
+        }
     }
 }
-
